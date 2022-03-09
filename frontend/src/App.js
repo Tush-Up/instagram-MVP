ab8d065783b87f207cb0fa94aacec1607836fd5c
Joy Okwudire <okwudirejoy@gmail.com>
2022-03-09 23:26:05 +0100

    fix login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import Signup from './pages/Signup/Signup';
+import React from "react";
+import Signup from "./pages/Signup/Signup";
 import Main from "./pages/Main";
-import LoginPage from './pages/LoginPage';
+import Login from "./pages/Login/Login";
+import Auth from "./pages/Auth/Auth";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-      // <Routes>
-      //   <Route path="/login" element={<Main />} />
-      //   <Route path="/" element={<LoginPage />} />
-      // </Routes>
-      <Signup/>
+    // <Routes>
+    //   <Route path="/login" element={<Main />} />
+    //   <Route path="/" element={<Login />} />
+    //   <Route path="/signup" element={<Signup />} />
+    // </Routes>
+    <Auth/>
   );
 }
 
---
+++ frontend/src/pages/Login/Login.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import './Login.css';
+import LeftBg from '../../components/LeftBg/LeftBg'
+// import Faeye from 'react-icons/lib/fa/eye';
+import appStoreImg from "../../assets/appstore.png";
+import playStoreImg from "../../assets/playstore.png";
+import fbIcon from "../../assets/fb-blue.png";
+const Login = (question, action) => {
+  return (
+    <div className="signup-right">
+      <div className="signup-main">
+        <p>Login to see photos and videos from your friends</p>
+
+        <form action="">
+          <div className="inputs">
+            <div>
+              <input type="text" placeholder="Mobile Number or Email" />
+            </div>
+
+            <div>
+              <input type="password" placeholder="Password" />
+            </div>
+          </div>
+
+          <button className="submit-btn" type="submit">
+            Log in
+          </button>
+        </form>
+
+        <div className="lines">
+          <div className="line"></div> <span>OR</span>{" "}
+          <div className="line"></div>
+        </div>
+
+        <button className="login-fb-btn">
+          <img src={fbIcon} alt="facebook icon" /> Log in with Facebook
+        </button>
+
+        <div className="get-app">
+          <p>Get the app</p>
+          <div>
+            <img src={playStoreImg} alt="Playstore" />
+            <img src={appStoreImg} alt="Appstore" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login
