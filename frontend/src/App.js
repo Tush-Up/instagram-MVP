aa4afcca2515ffe71139750ea107b286604f6859
glowwEE <gloryokafor5@gmail.com>
2022-03-09 11:08:35 +0100

    add Sidebar,Footer,Suggestions,Profile,ProfileIcon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Nav from '../../frontend/src/pages/Nav/Nav';
+import Sidebar from './pages/sidebar/Sidebar';
 
 function App() {
   return (
     <>
       <Nav />
+      <Sidebar />
    </>
   );
 }
