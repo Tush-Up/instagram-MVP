6d89d9d9582f8185dc7cbc64326c2a8b0194fc7a
Joy Okwudire <okwudirejoy@gmail.com>
2022-03-10 02:00:42 +0100

    dummy login to feeds

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,11 +1,34 @@
-import React from 'react'
-import './Login.css';
-import LeftBg from '../../components/LeftBg/LeftBg'
+import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
+import "./Login.css";
+import LeftBg from "../../components/LeftBg/LeftBg";
 // import Faeye from 'react-icons/lib/fa/eye';
 import appStoreImg from "../../assets/appstore.png";
 import playStoreImg from "../../assets/playstore.png";
 import fbIcon from "../../assets/fb-blue.png";
-const Login = (question, action) => {
+const Login = ({ question, action }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email && password) {
+      setTimeout(() => {
+        // user = {
+        //   email: password,
+        //   password: email,
+        //   isLogged: true,
+        // };
+        // localStorage.setItem("re-user", JSON.stringify(user));
+        navigate("/feeds");
+      }, 2000);
+    }
+  };
+
   return (
     <div className="signup-right">
       <div className="signup-main">
@@ -17,18 +40,30 @@ const Login = (question, action) => {
               <input
                 type="text"
                 placeholder="Mobile Number or Email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 required
               />
             </div>
 
             <div>
-              <input type="password" placeholder="Password" required />
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
             </div>
           </div>
 
-          <button className="submit-btn" type="submit">
-            Log in
-          </button>
+         
+            <button className="submit-btn" type="submit" onClick={handleSubmit}>
+              Log in
+            </button>
+         
         </form>
 
         <div className="lines">
@@ -50,6 +85,6 @@ const Login = (question, action) => {
       </div>
     </div>
   );
-}
+};
 
-export default Login
+export default Login;
